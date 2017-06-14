11cb4c4646d9b757eb9b38c4f1847c46e8d0bd10
Forbes Lindesay <forbes@lindesay.co.uk>
2017-06-14 15:15:58 +0100

    Continue renaming things

--- packages/create-react-app/createMopedApp.js
+++ packages/create-react-app/createMopedApp.js
@@ -11,21 +11,21 @@
 //   /!\ DO NOT MODIFY THIS FILE /!\
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// create-react-app is installed globally on people's computers. This means
+// moped is installed globally on people's computers. This means
 // that it is extremely difficult to have them upgrade the version and
 // because there's only one global version installed, it is very prone to
 // breaking changes.
 //
-// The only job of create-react-app is to init the repository and then
-// forward all the commands to the local version of create-react-app.
+// The only job of moped is to init the repository and then
+// forward all the commands to the local version of moped.
 //
 // If you need to add a new command, please add it to the scripts/ folder.
 //
 // The only reason to modify this file is to add more warnings and
-// troubleshooting information for the `create-react-app` command.
+// troubleshooting information for the `moped` command.
 //
 // Do not make breaking changes! We absolutely don't want to have to
-// tell people to update their global version of create-react-app.
+// tell people to update their global version of moped.
 //
 // Also be careful with new language features.
 // This file must work on Node 6+.
@@ -63,7 +63,7 @@ const program = new commander.Command(packageJson.name)
   .option('--verbose', 'print additional logs')
   .option(
     '--scripts-version <alternative-package>',
-    'use a non-standard version of react-scripts'
+    'use a non-standard version of moped-scripts'
   )
   .allowUnknownOption()
   .on('--help', () => {
@@ -74,14 +74,10 @@ const program = new commander.Command(packageJson.name)
     );
     console.log(`      - a specific npm version: ${chalk.green('0.8.2')}`);
     console.log(
-      `      - a custom fork published on npm: ${chalk.green(
-        'my-react-scripts'
-      )}`
+      `      - a custom fork published on npm: ${chalk.green('my-moped-scripts')}`
     );
     console.log(
-      `      - a .tgz archive: ${chalk.green(
-        'https://mysite.com/my-react-scripts-0.8.2.tgz'
-      )}`
+      `      - a .tgz archive: ${chalk.green('https://mysite.com/my-moped-scripts-0.8.2.tgz')}`
     );
     console.log(
       `    It is not needed unless you specifically want to use a fork.`
@@ -91,9 +87,7 @@ const program = new commander.Command(packageJson.name)
       `    If you have any problems, do not hesitate to file an issue:`
     );
     console.log(
-      `      ${chalk.cyan(
-        'https://github.com/facebookincubator/create-react-app/issues/new'
-      )}`
+      `      ${chalk.cyan('https://github.com/mopedjs/moped/issues/new')}`
     );
     console.log();
   })
@@ -106,7 +100,7 @@ if (typeof projectName === 'undefined') {
   );
   console.log();
   console.log('For example:');
-  console.log(`  ${chalk.cyan(program.name())} ${chalk.green('my-react-app')}`);
+  console.log(`  ${chalk.cyan(program.name())} ${chalk.green('my-moped-app')}`);
   console.log();
   console.log(
     `Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`
@@ -138,6 +132,20 @@ createApp(
 );
 
 function createApp(name, verbose, version, template) {
+
+  const useYarn = shouldUseYarn();
+  if (!useYarn) {
+    const npmInfo = checkNpmVersion();
+    if (!npmInfo.hasMinNpm) {
+      console.log(
+        chalk.red(
+          `You are using npm ${npmInfo.npmVersion}, moped requires npm v3 or higher.\n`
+        )
+      );
+      process.exit(1);
+    }
+  }
+
   const root = path.resolve(name);
   const appName = path.basename(root);
 
@@ -151,7 +159,7 @@ function createApp(name, verbose, version, template) {
     process.exit(1);
   }
 
-  console.log(`Creating a new React app in ${chalk.green(root)}.`);
+  console.log(`Creating a new Moped app in ${chalk.green(root)}.`);
   console.log();
 
   const packageJson = {
@@ -166,43 +174,11 @@ function createApp(name, verbose, version, template) {
   const originalDirectory = process.cwd();
   process.chdir(root);
 
-  if (!semver.satisfies(process.version, '>=6.0.0')) {
-    console.log(
-      chalk.yellow(
-        `You are using Node ${process.version} so the project will be bootstrapped with an old unsupported version of tools.\n\n` +
-          `Please update to Node 6 or higher for a better, fully supported experience.\n`
-      )
-    );
-    // Fall back to latest supported react-scripts on Node 4
-    version = 'react-scripts@0.9.x';
-  }
-
-  const useYarn = shouldUseYarn();
-  if (!useYarn) {
-    const npmInfo = checkNpmVersion();
-    if (!npmInfo.hasMinNpm) {
-      if (npmInfo.npmVersion) {
-        console.log(
-          chalk.yellow(
-            `You are using npm ${npmInfo.npmVersion} so the project will be boostrapped with an old unsupported version of tools.\n\n` +
-              `Please update to npm 3 or higher for a better, fully supported experience.\n`
-          )
-        );
-      }
-      // Fall back to latest supported react-scripts for npm 3
-      version = 'react-scripts@0.9.x';
-    }
-  }
   run(root, appName, version, verbose, originalDirectory, template, useYarn);
 }
 
 function shouldUseYarn() {
-  try {
-    execSync('yarnpkg --version', { stdio: 'ignore' });
-    return true;
-  } catch (e) {
-    return false;
-  }
+  return false;
 }
 
 function install(useYarn, dependencies, verbose, isOnline) {
@@ -260,7 +236,7 @@ function run(
   useYarn
 ) {
   const packageToInstall = getInstallPackage(version);
-  const allDependencies = ['react', 'react-dom', packageToInstall];
+  const allDependencies = [packageToInstall];
 
   console.log('Installing packages. This might take a couple of minutes.');
   getPackageName(packageToInstall)
@@ -274,9 +250,7 @@ function run(
       const isOnline = info.isOnline;
       const packageName = info.packageName;
       console.log(
-        `Installing ${chalk.cyan('react')}, ${chalk.cyan(
-          'react-dom'
-        )}, and ${chalk.cyan(packageName)}...`
+        `Installing ${chalk.cyan(packageName)}...`
       );
       console.log();
 
@@ -286,7 +260,11 @@ function run(
     })
     .then(packageName => {
       checkNodeVersion(packageName);
-      setCaretRangeForRuntimeDeps(packageName);
+
+      // Since moped-scripts has been installed with --save
+      // we need to move it into devDependencies and rewrite package.json
+      // also ensure moped has caret version range
+      fixDependencies(packageName);
 
       const scriptsPath = path.resolve(
         process.cwd(),
@@ -297,15 +275,6 @@ function run(
       );
       const init = require(scriptsPath);
       init(root, appName, verbose, originalDirectory, template);
-
-      if (version === 'react-scripts@0.9.x') {
-        console.log(
-          chalk.yellow(
-            `\nNote: the project was boostrapped with an old unsupported version of tools.\n` +
-              `Please update to Node >=6 and npm >=3 to get supported tools in new projects.\n`
-          )
-        );
-      }
     })
     .catch(reason => {
       console.log();
@@ -357,7 +326,7 @@ function run(
 }
 
 function getInstallPackage(version) {
-  let packageToInstall = 'react-scripts';
+  let packageToInstall = 'moped-scripts';
   const validSemver = semver.valid(version);
   if (validSemver) {
     packageToInstall += `@${validSemver}`;
@@ -425,7 +394,7 @@ function getPackageName(installPackage) {
         return packageName;
       })
       .catch(err => {
-        // The package name could be with or without semver version, e.g. react-scripts-0.2.0-alpha.1.tgz
+        // The package name could be with or without semver version, e.g. moped-scripts-0.2.0-alpha.1.tgz
         // However, this function returns package name only without semver version.
         console.log(
           `Could not extract the package name from the archive: ${err.message}`
@@ -442,8 +411,8 @@ function getPackageName(installPackage) {
       });
   } else if (installPackage.indexOf('git+') === 0) {
     // Pull package name out of git urls e.g:
-    // git+https://github.com/mycompany/react-scripts.git
-    // git+ssh://github.com/mycompany/react-scripts.git#v1.2.3
+    // git+https://github.com/mycompany/moped-scripts.git
+    // git+ssh://github.com/mycompany/moped-scripts.git#v1.2.3
     return Promise.resolve(installPackage.match(/([^\/]+)\.git(#.*)?$/)[1]);
   } else if (installPackage.indexOf('@') > 0) {
     // Do not match @scope/ when stripping off @version or @tag
@@ -485,7 +454,7 @@ function checkNodeVersion(packageName) {
     console.error(
       chalk.red(
         'You are running Node %s.\n' +
-          'Create React App requires Node %s or higher. \n' +
+          'Moped requires Node %s or higher. \n' +
           'Please update your version of Node.'
       ),
       process.version,
@@ -509,8 +478,10 @@ function checkAppName(appName) {
   }
 
   // TODO: there should be a single place that holds the dependencies
-  const dependencies = ['react', 'react-dom', 'react-scripts'].sort();
-  if (dependencies.indexOf(appName) >= 0) {
+  const dependencies = [];
+  const devDependencies = ['moped-scripts'];
+  const allDependencies = dependencies.concat(devDependencies).sort();
+  if (allDependencies.indexOf(appName) >= 0) {
     console.error(
       chalk.red(
         `We cannot create a project called ${chalk.green(
@@ -562,15 +533,18 @@ function setCaretRangeForRuntimeDeps(packageName) {
     process.exit(1);
   }
 
-  makeCaretRange(packageJson.dependencies, 'react');
-  makeCaretRange(packageJson.dependencies, 'react-dom');
+  packageJson.devDependencies = packageJson.devDependencies || {};
+  packageJson.devDependencies[packageName] = packageVersion;
+  delete packageJson.dependencies[packageName];
+
+  makeCaretRange(packageJson.devDependencies, packageName);
 
   fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
 }
 
 // If project only contains files generated by GH, it’s safe.
 // We also special case IJ-based products .idea because it integrates with CRA:
-// https://github.com/facebookincubator/create-react-app/pull/368#issuecomment-243446094
+// https://github.com/facebookincubator/moped/pull/368#issuecomment-243446094
 function isSafeToCreateProjectIn(root) {
   const validFiles = [
     '.DS_Store',
--- packages/create-react-app/index.js
+++ packages/create-react-app/index.js
@@ -13,21 +13,21 @@
 //   /!\ DO NOT MODIFY THIS FILE /!\
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// create-react-app is installed globally on people's computers. This means
+// moped is installed globally on people's computers. This means
 // that it is extremely difficult to have them upgrade the version and
 // because there's only one global version installed, it is very prone to
 // breaking changes.
 //
-// The only job of create-react-app is to init the repository and then
-// forward all the commands to the local version of create-react-app.
+// The only job of moped is to init the repository and then
+// forward all the commands to the local version of moped.
 //
 // If you need to add a new command, please add it to the scripts/ folder.
 //
 // The only reason to modify this file is to add more warnings and
-// troubleshooting information for the `create-react-app` command.
+// troubleshooting information for the `moped` command.
 //
 // Do not make breaking changes! We absolutely don't want to have to
-// tell people to update their global version of create-react-app.
+// tell people to update their global version of moped.
 //
 // Also be careful with new language features.
 // This file must work on Node 0.10+.
@@ -44,13 +44,13 @@ var currentNodeVersion = process.versions.node;
 var semver = currentNodeVersion.split('.');
 var major = semver[0];
 
-if (major < 4) {
+if (major < 6) {
   console.error(
     chalk.red(
       'You are running Node ' +
         currentNodeVersion +
         '.\n' +
-        'Create React App requires Node 4 or higher. \n' +
+        'Moped requires Node 6 or higher. \n' +
         'Please update your version of Node.'
     )
   );
